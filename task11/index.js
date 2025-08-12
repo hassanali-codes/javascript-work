57fe0cd31fc74f9a8fdb092f16d59df7b03127fb
adnanmusil <adnan.museel123@gmail.com>
2025-08-12 17:33:51 +0500

    Doccument object and its properties

---
+++ task11/index.js
@@ -0,0 +1,10 @@
+// Document Object and its properties:
+// The document object is your entry point to the DOM.
+
+console.log(document.documentElement)
+console.log(document.head)
+console.log(document.body); // shows <body>...</body>
+console.log(document.title); // shows "My Page"
+
+
+
