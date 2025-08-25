b5719e7465d576263cc51a37293c9f8941a865cf
adnanmusil <adnan.museel123@gmail.com>
2025-08-25 21:59:33 +0500

    program to change button hove color

--- task11/index.js
+++ task11/index.js
@@ -1,47 +1,14 @@
-// ---------- PART #2 DOM --------------
-// Properties: there are some properties you can get or set(change) or update
-// .tagName
-// .innertwxt
-// .innerHTML
-// .textcontent
+const myButton = document.getElementById("myButton")
+
+myButton.classList.add("enabled")
+myButton.classList.remove("enabled")
+myButton.classList.add("hover")
 
 
-// ATTRIBUTES:
-// getAttribute
+myButton.addEventListener("mouseover", event => {
+    event.target.classList.toggle("hover");
+})
 
-let div = document.querySelector("div")
-
-let id= div.getAttribute("id")
-console.log(id)
-
-let a = document.querySelector("div")
-
-let val = div.getAttribute("name")
-console.log(val)
-
-let para = document.querySelector("p")
-console.log(para.getAttribute("class"))
-
-console.log(para.setAttribute("class", "hasan"))
-
-div.setAttribute(div, "hassan")
-
-
-
-// ------- .style method ----------
-let dv = document.querySelector("div")
-
-div.style.backgroundColor = "purple"
-
-div.style.fontSize = "24px"
-
-div.innerText = "Hello From Javascript"
-
-// ---------add/remove----------
-
-let para1 = document.querySelector("p")
-
-para1.classList.add("newClass", "font")
-
-
-para1.classList.remove("newClass")
+myButton.addEventListener("mouseout", event => {
+    event.target.classList.toggle("hover");
+});
