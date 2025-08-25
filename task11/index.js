b69c76b56e9ced837cb20e64954eb70c9b490bde
adnanmusil <adnan.museel123@gmail.com>
2025-08-25 22:01:14 +0500

    js event listener

--- task11/index.js
+++ task11/index.js
@@ -1,17 +1,47 @@
+// --------------EVENTS IN JAVASCRIPT---------------------
+// tThe change in the state of ana object is known as event
 
-// ---------------TO DO LIST APP---------------------
+let Btn = document.querySelector("#btn1")
 
-let btn = document.getElementById("addBtn");
+Btn.onclick = (evt) => {
+  console.log("btn1 was clicked")
+  let a = 25;
+  a++;
+  console.log(a)
+  console.log(evt)
+  console.log(evt.type)
+  console.log(evt.target)
+  console.log(evt.clientX, evt.clientY)
+}
 
-    btn.addEventListener("click", () => {
-      let input = document.querySelector("input")
-      if(!input.value.trim())
-          return;
 
-      let li = document.createElement("li");
-      li.innerHTML = `${input.value} <button>Delete</button>`;
-      li.querySelector("button").onclick = () => li.remove();
+let box = document.querySelector("div")
+box.onmouseover = () => {
+  console.log("you are inside the div")
+}
 
-      document.getElementById("taskList").appendChild(li);
-      input.value= ""; 
-    })
+
+// ----------Event Listeners---------------
+Btn.addEventListener("click", (evt) => {
+  console.log("button1 was clicked - handler1")
+//   console.log(evt)
+//   console.log(evt.type)
+})
+
+Btn.addEventListener("click", () => {
+  console.log("button1 was clicked - handler2")
+})
+
+// Note: the callback reference should be same to remove
+const handler3 = () => {
+  console.log("button1 was clicked - handler3")
+}
+
+Btn.addEventListener("click", handler3);
+
+Btn.addEventListener("click", () => {
+  console.log("button1 was clicked - handler4")
+})
+
+// Note: the callback reference should be same to remove
+Btn.removeEventListener("click", handler3)
