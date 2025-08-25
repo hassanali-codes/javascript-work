ab217a49ff5f573f61523f54cbdd75cd99e71a9e
adnanmusil <adnan.museel123@gmail.com>
2025-08-25 13:13:54 +0500

    DOM NAVIGATION

--- task11/index.js
+++ task11/index.js
@@ -1,172 +1,76 @@
-<<<<<<< HEAD
-// ----Practice question1 -------
-const firstPara = document.querySelector(".intro")
-console.log(firstPara.textContent)
-=======
-// DOM NAVIGATION = The process of navigating through the structure of an HTML document using javascipt:
+console.log("hello")
+// alert("Hello Wolrd")
 
-// .parentElement
-// .children
+// WINDOW OBJECT:
+console.log("Hello")
+// window.alert("hello2")
 
-// ------.parentElement---------
- const parElement = document.getElementById("pie")
- const parent = parElement.parentElement;
- parent.style.backgroundColor = "yellow"
+// DOM (DOCUMENT OBJECT MODEL):
+console.dir(document.body.childNodes[1])
+console.log(document.body)
 
-//--------.children------------
-const childElement = document.getElementById("vegetables")
-const child = childElement.children;
+// 2. DOM MANIPULATION:
 
-// console.log(child)
-
-child[2].style.backgroundColor = "yellow"
-
- Array.from(child).forEach(child => {
-     child.style.backgroundColor = "yellow"
- })
+// By ID
+let head = document.getElementById("heading")
+console.log(head)
 
 
+// By Class
+let headings = document.getElementsByClassName("head-class")
+console.log(headings)
+console.dir(headings)
+
+// By Tag:
+let paras = document.getElementsByTagName("p")
+console.log(paras)
+console.dir(paras)
 
 
+// Query Selector:
+let elements = document.querySelector(".head-class")  //return 1st element.can access id and class both.
+console.dir(elements)
+
+let allEl = document.querySelectorAll(".head-class")   // all elements
+console.dir(allEl) 
+
+//firstElementChild
+// lastElementChild
+// nextElementSibling
+// previousElementSibling
+// parentElement
+// childrens
 
 
+ 
+const elmnts = document.getElementById("vegetables")
+const firstChild = elmnts.firstElementChild;
+firstChild.style.backgroundColor = "yellow"
 
 
+const ulElements = document.querySelectorAll("ul")
 
-
-
-
-
-
-
->>>>>>> f7f977ffde2c1b38876cb1f3aca4a810fa28a22f
-
-const allPara = document.querySelectorAll(".intro")
-allPara.forEach((p, index) => {
-    console.log(`paragraph ${index + 1}:`, p.textContent)
+ulElements.forEach(ulElements => {
+    const firstChild = ulElements.firstElementChild;
+    firstChild.style.backgroundColor = "yellow"
 })
 
-const fruitList = document.getElementById("fruit-list")
-console.log("first fruit: ",fruitList.firstElementChild.textContent)
-console.log("last fruit: ", fruitList.lastElementChild.textContent)
-
-console.log(fruitList.textContent)
-
-
-//  Changing Text in Elements
-
-
-let element = document.createElement("p"); // Step 1: create
-element.innerText = "Hello World!";         // Step 2: add text
-document.body.appendChild(element);         // Step 3: put on page
+let header = document.querySelector("h1")
+console.log(header.textContent)
 
 
 
-element.textContent = "Hello from textcontent!"
-element.innerHTML = "<b>Bold Text</b> and normal text";
-
-// //  Changing Attributes
-
-let img = document.querySelector("img");
-console.log(img.getAttribute("src")); // cat.jpg
-
-img.setAttribute("width", "500");
-
-// //Changing Styles
-
-element.style.color = "red";
-element.style.backgroundColor = "yellow";
-
-// Adding / Removing Classes
-title.classList.add("highlight"); // Adds a class
-title.classList.remove("highlight"); // Removes a class
-title.classList.toggle("highlight"); // Adds if not present, removes if present
-
-
-//  Mini Practice: Change background on button click 
-// ------------ Problem No 2 -------------
-function changeBG() {
-    document.getElementById("title").classList.toggle("highlight");
-  }
-
-
-console.log(document.getElementById("title").classList);
-
-
-// . Create an Element
-const newDiv = document.createElement("div")
-newDiv.textContent = "Hey I am new Here"
-
-// Append element
-
-document.body.appendChild(newDiv)
-
-
-// Remove element
-
-newDiv.remove();
-
-//  Replace element 
-
-const newHeading = document.createElement("h2");
-newHeading.textContent = "I replaced the old heading!";
-document.body.replaceChild(newHeading, document.querySelector("h1"));
 
 
 
-// ------------------TO DO LIST APP------------------------------
-const input = document.getElementById("taskInput");
-  const btn = document.getElementById("addBtn");
-  const list = document.getElementById("taskList");
-
-  btn.addEventListener("click", () => {
-    if (input.value.trim() === "") return;
-
-    // Create list item
-    const li = document.createElement("li");
-    li.textContent = input.value + " "; // Add some space before button
-
-    // Create delete button
-    const delBtn = document.createElement("button");
-    delBtn.textContent = "❌";
-    delBtn.style.marginLeft = "10px";
-    delBtn.addEventListener("click", () => li.remove());
-
-    // Add delete button inside li
-    li.appendChild(delBtn);
-
-    // Add li to the list
-    list.appendChild(li);
-
-    // Clear input box
-    input.value = "";
-  });
-
-
- document.getElementById("colors").addEventListener("click", (e) => {
-    if (e.target.tagName === "LI") {
-      e.target.style.backgroundColor = "yellow";
-    }
-  });
 
 
 
-//--------------- leet code problem --------------
-let count = 0;
-  const display = document.getElementById("count");
 
-  document.getElementById("inc").addEventListener("click", () => {
-    count++;
-    display.textContent = count;
-  });
 
-  document.getElementById("dec").addEventListener("click", () => {
-    count--;
-    display.textContent = count;
-  });
 
-  document.getElementById("reset").addEventListener("click", () => {
-    count = 0;
-    display.textContent = count;
-  });
+
+
+
+
 
