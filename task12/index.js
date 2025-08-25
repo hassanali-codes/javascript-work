f3ab1abfdb97ce05cacd5231d03c550a85fc02bc
adnanmusil <adnan.museel123@gmail.com>
2025-08-26 01:12:15 +0500

    mouse and keyboard events

--- task12/index.js
+++ task12/index.js
@@ -1,19 +1,53 @@
 // ----------javascript Events------------
+const mybutton = document.getElementById("myBtn")
 let button = document.getElementById("btn")
 
-button.addEventListener("click", () => {
-    alert("Button clicked with addEventlistener")
+ mybutton.addEventListener("mouseover", event => {
+    button.style.backgroundColor = "yellow"
+    button.textContent = "Don't do it 😲"
 })
- const myButton = document.getElementById("btn")
 
-
-const mybutton = document.getElementById("myBtn")
-const buton = document.getElementById("myBox")
-
-mybutton.addEventListener("click", event => {
-    buton.style.backgroundColor = "tomato"
-    buton.textContent = " OUCH!🤕"
+mybutton.addEventListener("mouseout", event => {
+    button.style.backgroundColor = "lightgreen"
+    button.textContent = "Click Me 😊"
 })
+
+// Hover Button
+
+const myButton = document.getElementById("myButton")
+
+myButton.classList.add("enabled")
+myButton.classList.remove("enabled")
+myButton.classList.add("hover")
+
+
+myButton.addEventListener("mouseover", event => {
+    event.target.classList.toggle("hover");
+})
+
+myButton.addEventListener("mouseout", event => {
+    event.target.classList.toggle("hover");
+});
+
+
+
+  let input = document.getElementById("myInput");
+
+  // Key Down
+  input.addEventListener("keydown", (event) => {
+    console.log("Key down:", event.key);
+  });
+
+  // Key Up
+  input.addEventListener("keyup", (event) => {
+    console.log("Key up:", event.key);
+  });
+
+  // Key Press (older, not recommended)
+  input.addEventListener("keypress", (event) => {
+    console.log("Key pressed:", event.key);
+  });
+
 
 
 
