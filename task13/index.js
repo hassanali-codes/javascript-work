e46e40938482585e63088eedf3990a89685e5ecf
adnanmusil <adnan.museel123@gmail.com>
2025-08-26 01:48:02 +0500

    practice and leet code problem

--- task13/index.js
+++ task13/index.js
@@ -1,66 +1,45 @@
 // --------------TASK 13------------
-// Spread & Rest Operators
-const d = [1,2,3]
-const e = [4, 5]
+// Create a function using rest that sums unlimited numbers
+function Sum(...numbers){
 
-const copy = [...d]
-console.log(copy)
+  let result = 0;
+  for(let number of numbers){
+    result += number;
+  }
+  return result;
 
-const merged = [...d, ...e]
-console.log(merged)
-
-const middle = [0, ...d, ...e, 100]
-console.log(middle)
-
-const nums = [3, 8, 1]
-console.log(Math.max(...nums))
-console.log(Math.min(...nums))
-
-console.log([...`Ali`]) // string: arra of characters
-
-// In Objects
-const nameId = {id: 1, name: 'Hassan', city: "lahore"}
-const extra = {city: "Karachi", name: "Ali"}
-
-const copyObj = {...nameId}
-
-console.log(copyObj)
-
-// merge (later keys overwrite earlier ones)
-const mergeObj = {...nameId, ...extra}
-console.log(mergeObj)
-
-
-// REST OPERATORS:
-// allow a function work with a variable number of arguments by bundling them into an array
-
-function fruits(...fruit){
-  console.log(fruit)
-  console.log(...fruit)  //also use spread to seperate each elements
 }
 
-const fruit1 = "mango"
-const fruit2 = "apple"
-const fruit3 = "peach"
+const total = Sum(1,2,3,4,5,6,7,8,9,10)
+console.log(`your total is $${total}`) 
 
 
-fruits(fruit1, fruit2, fruit3) 
-
-
-function getFood(...foods){
-  return foods
+function sum(...numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
 }
 
-const foods = getFood(fruit1, fruit2, fruit3)
-console.log(foods)
+console.log(sum(1, 2, 3, 4)); // 10
 
 
-// Merge two arrays using spread
-let arr1 = [1,2,3,4,5]
-let arr2 = [5,6,7,8,9]
 
-let mergd = [...arr1, ...arr2]
-console.log(...mergd)
+
+// --------------Leet Code Style Problem--------------
+// Problem: Write a function twoSum(nums, target) that returns the indices of the two numbers that add up to target.
+
+function twoSums(nums, target){
+  for(let i=0; i < nums.length; i++){
+    for(let j=0; j<nums.length; j++){
+      if(nums[i] + nums[j] === target){
+        return [i, j]
+      }
+    }
+  }
+}
+
+console.log(twoSums([1,2,3,4,5,6], 3))
+
+
+
 
 
 
