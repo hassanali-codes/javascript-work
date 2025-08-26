c521e9b7d93075757b91ba5e43924231724d276b
adnanmusil <adnan.museel123@gmail.com>
2025-08-27 02:45:24 +0500

    Intro to Algorithms

---
+++ task14/index.js
@@ -0,0 +1,39 @@
+// task 14 - Binary and linear search
+// Algorithms are step-by-step procedures or formulas for solving a problem or performing a task. They are essential in computer science for 
+// data processing, // calculations, and other automated tasks.
+
+// Efficiency:
+`.) Time complexity tells us how fast or slow an algorithm runs as the input size grows.
+O(n): Linear time complexity - the time taken grows linearly with the input size.
+O(log n): Logarithmic time complexity - the time taken grows logarithmically with the input size.
+
+Example: searching an item in a list of 1000000 numbers
+.) Linear Search (O(n)):may take upto 1000000 steps
+.) Binary Search (O(log n)): may take upto 20 steps (if the list is sorted)
+
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
