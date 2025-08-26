22967e953055620cc446db52662ccb0b6cb70745
adnanmusil <adnan.museel123@gmail.com>
2025-08-27 03:03:56 +0500

    Binary Search Algorithm

--- task14/index.js
+++ task14/index.js
@@ -1,23 +1,31 @@
-// linear search algorithm:
-`1. Iterate through each element of the array.
-2. Compare the current element with the target value.
-3. If a match is found, return the index of the element.
-4. If the end of the array is reached without finding the target, return -1 
-to indicate that the target is not present in the array.`
+// Binary Search Algorithm (only works on sorted arrays)
 
+` 1. find the middle element of the array
+  2. if the middle element = target -> return the index
+  3. if target < middle -> search in the left half
+  4. if target > middle -> search in the right half
+  5. repeat until the target is found or the array is empty`
 
-function linearSearch(arr, target) {
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] === target){
-            return i
+function binarySearch(arr, target) {
+    let low = 0;
+    let high = arr.length -1;
+
+    while (low <= high){
+        let mid = Math.floor((low + high) / 2);
+        if(arr[mid] === target){
+            return mid;
+        }
+        else if(arr[mid] < target){
+            low =  mid + 1
+        }
+        else{
+            high = mid - 1
         }
     }
     return -1
 }
 
-console.log(linearSearch([1,2,3,4,5], 6))
-
-
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,13,1000,2000,3000,4000], 2000)) 
 
 
 
