01de3e1ba0e282ccb0bd6fd318b44985bdbc9cca
adnanmusil <adnan.museel123@gmail.com>
2025-08-27 19:43:56 +0500

    problem2&3

--- task15/index.js
+++ task15/index.js
@@ -1,37 +1,36 @@
-// task 15
-//  Task: Create one function declaration and one function expression,
-// and test calling them before they’re defined.
+// Practice Problem 2
+//2.	Write a function that uses both global and local variables to calculate the sum 
 
-declarationFunc(); 
-// expressionFunc(); 
-function declarationFunc() {
-    console.log("This is a function declaration.");
+let a = 10; 
+function LocalglobalSum(b) { 
+    return a + b; 
+
+}
+console.log(LocalglobalSum(5)); 
+
+
+// Practice Problem 3
+// 	Write code that demonstrates hoisting with functions and variables, then explain the output line by line.
+
+console.log(alpha)
+var alpha = "Hello World"
+console.log(alpha)
+
+console.log(beta)
+let beta = "Hello"
+console.log(beta)
+
+greet();   // "Hello!"  (function declaration hoisted)
+
+function greet() {
+    console.log("Hi there!");
 }
 
-// const expressionFunc = function() {
-//     console.log("This is a function expression.");
-// };
+sayBye();  // TypeError: sayBye is not a function (if sayBye declared with var later)
 
-// Practice Problems:1.	Write a program to check if a variable inside a loop is accessible outside the loop 
-// when declared with var, let, and const.
-
-for (var i = 0; i < 3; i++) {
-    console.log("Inside loop with var:", i);
+var sayBye = function() {
+    console.log("Bye!");
 }
-console.log("Outside loop with var:", i); 
-
-for (let j = 0; j < 3; j++) {}
-console.log("Outside loop with let:", j); // ReferenceError. J is not defined
-
-for (const k = 0; k < 3; k++) {}
-console.log("Outside loop with const:", k); // ReferenceError. K is not defined
-
-
-
-
-
-
-
 
 
 
