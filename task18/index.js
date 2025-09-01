c47734968f2d8eaf43f93acb57d7e1a5d8909c60
adnanmusil <adnan.museel123@gmail.com>
2025-09-02 02:12:45 +0500

    Practice Task

--- task18/index.js
+++ task18/index.js
@@ -1,20 +1,19 @@
 // ----------------Task 18----------------
-// Post request with Fetch
-fetch("https://jsonplaceholder.typicode.com/posts",{
-    method : "POST",
-    body : JSON.stringify({
-        tittle: "sending data",
-        body : "Hello world",
-        userId : 1
-    }),
-    headers : {
-        "content-type" : "applcation/JSON; charset=UTF-8"
-    }
+// Create a new post with your own custom title/body and log the response.
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",   // method: POST
+  body: JSON.stringify({   // data we want to send
+    title: "Task18 Js",
+    body: "Created a new post with my own custom tittle/body",
+    userId: 804
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8" // telling server it's JSON
+  }
 })
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data)); 
 
-  
 
 
 
