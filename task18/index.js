4bedae9d412054366f6fc99b047e50fc609b5ef4
adnanmusil <adnan.museel123@gmail.com>
2025-09-02 01:44:24 +0500

    task3

--- task18/index.js
+++ task18/index.js
@@ -1,11 +1,25 @@
 // ----------------Task 18----------------
-//   Fetch a single post (/posts/1) and log it.
+// Try a wrong URL and handle the error gracefully.
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then(response => response.json())
-  .then(posts => {
-    console.log("single post", posts[0])
+  .then(data => console.log(data))
+  .catch(error => console.error(`Error`, error))    
+
+
+fetch("https://jsonplaceholder.typicode.com/wrongendpoint") // wrong URL
+  .then(response => {
+    if (!response.ok) {
+      // This will run if server returns 404, 500, etc.
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+    return response.json();
   })
-  .catch(error => console.error("Error:", error));
+  .then(data => {
+    console.log("Data received:", data);
+  })
+  .catch(error => {
+    console.error("Fetch failed:", error.message);  
+  });
 
   
 
