f3e1793e6397bedf7b37cb2341c21d900ac13176
adnanmusil <adnan.museel123@gmail.com>
2025-09-02 02:19:15 +0500

    Async/Await with Fetch

--- task18/index.js
+++ task18/index.js
@@ -1,21 +1,13 @@
-// ----------------Task 18----------------
-// Create a new post with your own custom title/body and log the response.
-fetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "POST",   // method: POST
-  body: JSON.stringify({   // data we want to send
-    title: "Task18 Js",
-    body: "Created a new post with my own custom tittle/body",
-    userId: 804
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8" // telling server it's JSON
-  }
-})
-  .then(response => response.json())
-  .then(data => console.log(data)); 
-
-
-
-
-
-       
+// Task 18
+// Async/Await with Fetch
+async function Post() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const data = await response.json()
+        console.log(data.slice(0, 5))
+    }
+    catch (error){
+        console.error("Error", error)
+    }
+}
+Post()
