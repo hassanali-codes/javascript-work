5b42d9e9212accfdb808ff0ec621c25f15f3237f
adnanmusil <adnan.museel123@gmail.com>
2025-09-02 02:06:54 +0500

    post request with fetch

--- task18/index.js
+++ task18/index.js
@@ -1,25 +1,18 @@
 // ----------------Task 18----------------
-// Try a wrong URL and handle the error gracefully.
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(`Error`, error))    
-
-
-fetch("https://jsonplaceholder.typicode.com/wrongendpoint") // wrong URL
-  .then(response => {
-    if (!response.ok) {
-      // This will run if server returns 404, 500, etc.
-      throw new Error(`HTTP Error! Status: ${response.status}`);
+// Post request with Fetch
+fetch("https://jsonplaceholder.typicode.com/posts",{
+    method : "POST",
+    body : JSON.stringify({
+        tittle: "sending data",
+        body : "Hello world",
+        userId : 1
+    }),
+    headers : {
+        "content-type" : "applcation/JSON; charset=UTF-8"
     }
-    return response.json();
-  })
-  .then(data => {
-    console.log("Data received:", data);
-  })
-  .catch(error => {
-    console.error("Fetch failed:", error.message);  
-  });
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
 
   
 
