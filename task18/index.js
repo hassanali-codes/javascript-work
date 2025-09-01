65f27e3dcd3ea34e503e573734da16ec6ef7a61a
adnanmusil <adnan.museel123@gmail.com>
2025-09-02 01:42:51 +0500

    task1

--- task18/index.js
+++ task18/index.js
@@ -1,8 +1,11 @@
 // ----------------Task 18----------------
+// Fetch all posts and print first 5 titles.
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(`Error`, error))    
+  .then(data => {
+    console.log("first five elements:", data.slice(0, 5))
+  })
+  .catch(error => console.error("Error:", error));
 
 
-
+       
