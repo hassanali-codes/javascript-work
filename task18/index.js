8d8ee8363d6aca28abee1610d4394978e152854e
adnanmusil <adnan.museel123@gmail.com>
2025-09-02 15:05:32 +0500

    leetcode task1(part2)

--- task18/index.js
+++ task18/index.js
@@ -1,21 +1,17 @@
 // TASK 18
 // ---------------tASK 1--------------------
 //Convert the previous fetch examples (GET posts, single post, POST new post) into async/await style.
-// GET ALL POSTS (first 5 titles)
-async function getAllpost(){
-    try{
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+// Get a Single post (/post/1)
+async function getSinglePost(){
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+        const Post = await response.json()
 
-    const data = await response.json();
-
-    console.log("First 5 post titles")
-    data.slice(0, 5).forEach(post => console.log(post.title)) // step3: Log the first 5 titles
+        console.log("Single Post: ", Post)
+    }
+    catch (error){
+        console.error("Error fetching single post:", error)
+    }
 }
-catch (error){
-    console.error("Error fetching posts:", error)
-}
-}
+getSinglePost()
 
-getAllpost()
-
-    
