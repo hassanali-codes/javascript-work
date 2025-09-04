c2179d9999fc4bde1e02e7f1d66b1360a16e14bd
adnanmusil <adnan.museel123@gmail.com>
2025-09-05 01:58:33 +0500

    exercise question 3&4

--- task20/index.js
+++ task20/index.js
@@ -1,26 +1,30 @@
 // -----------------TASK 20---------------------------
-// Task 2
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this._age = age;   
-  }
-
-  get age() {
-    return this._age;
-  }
-
-  set age(newAge) {
-    this._age = newAge;
+// Task 3
+class Student extends Person {
+  constructor(name, age, grade) {
+    super(name, age); 
+    this.grade = grade;
   }
 
   showInfo() {
-    console.log("Name: " + this.name + ", Age: " + this._age);
+    console.log("Student Name: " + this.name + ", Age: " + this._age + ", Grade: " + this.grade);
+  }
+
+// Task 4
+  static compare(s1, s2) {
+    if (s1.grade > s2.grade) {
+      return s1;
+    } else {
+      return s2;
+    }
   }
 }
 
-let p1 = new Person("Ali", 30);
-p1.showInfo();              
-console.log("Age:", p1.age); // getter
-p1.age = 31;                // setter
-console.log("Updated Age:", p1.age);
+let s1 = new Student("Hassan", 20, 85);
+let s2 = new Student("Ahmed", 21, 90);
+
+s1.showInfo();
+s2.showInfo();
+
+let topper = Student.compare(s1, s2);
+console.log("Topper is:", topper.name);
