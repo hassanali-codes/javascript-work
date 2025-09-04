65d1c840b7b03b04416af125e1e51bf3094706cb
adnanmusil <adnan.museel123@gmail.com>
2025-09-04 18:05:05 +0500

    Constructor in js

--- task20/index.js
+++ task20/index.js
@@ -1,35 +1,17 @@
 // -----------------TASK 20---------------------------
-// Class declaration vs expression
-// class declaration
-class Person{
-    constructor(name){
+// Constructor Methods: constructor() is a special method inside a class.It runs automatically when you create a new object.
+// Used to initialize properties (give them starting values).
+
+class Student {
+    constructor(name, grade){
         this.name = name
+        this.grade = grade
     }
-    greet(){
-        console.log(`Hello ${this.name}`)
-    }
-}
-
-const p1 = new Person("Hassan ali")
-
-p1.greet()
-
-// Named class expression
-const car = class CarClass{
-    constructor(brand){
-        this.brand = brand
+    showGrade(){
+        console.log(`NAME : ${this.name}`)
+        console.log(`GRADE : ${this.grade}`)
     }
 }
+const st1 = new Student("Hassan Ali", "A+")
 
-// Anonymous class expression (no class name inside)
-const bike = class {
-    constructor(model){
-        this.model = model
-    }
-}
-
-const c1 = new car("Toyota")
-const c2 = new bike("yamaha")
-
-console.log(c1.brand)
-console.log(c2.model)
+st1.showGrade()
