d691375a77cb4f148bda92b633a43652d8a70dd7
adnanmusil <adnan.museel123@gmail.com>
2025-09-05 01:50:02 +0500

    exercie question1

--- task20/index.js
+++ task20/index.js
@@ -1,27 +1,13 @@
 // -----------------TASK 20---------------------------
-class Rectangle {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-
-  // Getter
-  get area() {
-    return this.width * this.height;
-  }
-
-  // Setter
-  set changeWidth(newWidth) {
-    if (newWidth > 0) {
-      this.width = newWidth;
-    } else {
-      console.log("Width must be positive!");
+class car {
+    constructor(make, model, year) {
+        this.make = make
+        this.model = model
+        this.year = year
     }
-  }
+    getdetails() {
+        return ` ${this.make} \n Model no. ${this.model} \n year ${this.year}`
+    }
 }
-
-const r = new Rectangle(10, 5);
-console.log(r.area);  
-
-r.changeWidth = 20;   
-console.log(r.area);  
+const info = new car("BMW", "M4 COMP", "2024");
+console.log(info.getdetails())
