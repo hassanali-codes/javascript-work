e3f9ff242fd92445cbe4440ff7122394a26f868b
adnanmusil <adnan.museel123@gmail.com>
2025-09-11 19:13:55 +0500

    Practice task

--- task22/index.js
+++ task22/index.js
@@ -1,25 +1,20 @@
 // ----------Task 22--------------
-// ES6 Classes with prototype
-class Animal {
-    constructor(name){
-        this.name = name
-    }
-
-    speak(){
-        console.log(this.name, "makes a sound")
-    }
+// Practice Task(prototype)
+function Shape(width, height){
+    this.width = width
+    this.height = height
 }
-
-class Dog extends Animal {
-    constructor(name){
-        super(name)
-    }
-    speak(){
-        console.log(this.name,"barks")
-    }
+Shape.prototype.Area = function(){
+    console.log("Area")
 }
+function Rectangle(width, height){
+    Shape.call(this, width, height)
+}
+Rectangle.prototype = Object.create(Shape.prototype)
+Rectangle.prototype.constructor = Rectangle
 
-const dog1 = new Dog("german shephard")
-const animal = new Animal("rabbit")
-animal.speak()
-dog1.speak()
+Rectangle.prototype.Area = function () {
+    console.log(this.width * this.height)     
+}
+const r1 = new Rectangle(20, 6)
+r1.Area()
